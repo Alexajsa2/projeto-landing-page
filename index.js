58486b3865d864af812b5dbcf0bd66254fc3cb64
unknown <jralex_junio@hotmail.com>
2023-09-26 20:37:44 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha = window.document.getElementById("Samantha")
+var Bruna = window.document.getElementById("Bruna")
+var SetaDireita = window.document.getElementById("seta-direita")
+var SetaEsquerda = window.document.getElementById("seta-esquerda")
+
+function RolarParaDireita() {
+    Leonardo.style = "display:none"
+    Bruna.style = "display:flex"
+    SetaEsquerda.style = "display: flex; margin-top:55px"
+    SetaDireita.style = "display: none"
+}
+
+function RolarParaEsquerda() {
+    Leonardo.style = "display:flex"
+    Bruna.style = "display:none"
+    SetaEsquerda.style = "display: none"
+    SetaDireita.style = "display: flex margin-top:55px"
+}
